2466e6f79a06dd55c3c5fea8a3cb23b4bd85b572
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2023-07-09 19:17:19 +0200

    feat: Create balancer, implemented horizontal scaling

---
+++ src/app.ts
@@ -0,0 +1,85 @@
+/* eslint-disable class-methods-use-this */
+import {
+  IncomingMessage, ServerResponse, createServer, request,
+} from 'http';
+import cluster from 'cluster';
+import { cpus } from 'os';
+
+class App {
+  private PORT: number;
+
+  private server = createServer();
+
+  static currentPortIndex = 0;
+
+  static workerPorts: number[] = [];
+
+  constructor(PORT: number) {
+    this.PORT = PORT;
+  }
+
+  private balancer(req: IncomingMessage, res: ServerResponse) {
+    if (cluster.isPrimary) {
+      const options = {
+        port: App.workerPorts[App.currentPortIndex],
+        path: req.url,
+        method: req.method,
+        headers: req.headers,
+      };
+      const workerRequest = request(options, (workerReq) => {
+        res.writeHead(
+          Number(workerReq.statusCode),
+          workerReq.statusMessage,
+          workerReq.headers,
+        );
+        workerReq.pipe(res);
+
+        App.currentPortIndex === App.workerPorts.length - 1
+          ? App.currentPortIndex = 0
+          : App.currentPortIndex += 1;
+      });
+      req.pipe(workerRequest);
+    }
+  }
+
+  private handleRequest(req: IncomingMessage, res: ServerResponse) {
+    this.balancer(req, res);
+    if (cluster.isWorker) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      res.write(`I am ${process.pid}, port ${process.env.PORT}`);
+      res.end();
+    }
+  }
+
+  private multi() {
+    cpus().forEach((_, i) => {
+      const workerPort = this.PORT + 1 + i;
+      App.workerPorts.push(workerPort);
+
+      cluster.fork({ PORT: workerPort })
+        .on('exit', (worker) => {
+          console.log(`Worker ${worker} was killed`);
+        });
+    });
+  }
+
+  run() {
+    this.server.on('request', (req, res) => {
+      this.handleRequest(req, res);
+    });
+    this.server.listen(this.PORT, () => {
+      if (cluster.isPrimary && process.env.NODE_ENV === 'multi') {
+        console.log(`The server started listening to requests on port ${this.PORT}`);
+        this.multi();
+      } else if (cluster.isWorker) {
+        console.log(`Worker ${process.pid} started on port: ${process.env.PORT}`);
+      } else {
+        console.log(`The server started listening to requests on port ${this.PORT}`);
+      }
+    });
+  }
+}
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hi');
+import dotenv from 'dotenv';
+import App from './app';
+
+dotenv.config();
+
+const app = new App(Number(process.env.PORT));
+app.run();
